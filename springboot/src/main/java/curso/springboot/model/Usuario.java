8e0f1e97c6ff4f770d84d7a8e8d787cd5ba9b3c1
luangf <luangf222@gmail.com>
2023-03-22 10:54:48 -0300

    Spring Security + JPA + Banco de dados - Validando Usuário

---
+++ springboot/src/main/java/curso/springboot/model/Usuario.java
@@ -0,0 +1,84 @@
+package curso.springboot.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String login;
+	private String senha;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
--- springboot/src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ springboot/src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -1,5 +1,6 @@
 package curso.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,13 +9,15 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
 	@Override // Configura as solicitações de acesso por Http
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf()
@@ -29,10 +32,13 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override // Cria autenticação do usuário com banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-		.withUser("luan")
-		.password("$2a$10$U4hYraO2UaF83/AgdxGe8eIApnhc8GYP4FDHsc2x8WXv79jIX7bMS")
-		.roles("ADMIN");
+		auth.userDetailsService(implementacaoUserDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+//		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+//		.withUser("luan")
+//		.password("$2a$10$U4hYraO2UaF83/AgdxGe8eIApnhc8GYP4FDHsc2x8WXv79jIX7bMS")
+//		.roles("ADMIN");
 	}
 	
 	@Override // Ignora URL especificas
