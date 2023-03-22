402d7b6da1d668f142a26afd1ba08fc2527b20de
luangf <luangf222@gmail.com>
2023-03-22 15:04:39 -0300

    Spring Security + Thymeleaf + Páginas HTML -Controle de Acesso

--- springboot/src/main/java/curso/springboot/SpringbootApplication.java
+++ springboot/src/main/java/curso/springboot/SpringbootApplication.java
@@ -1,12 +1,10 @@
 package curso.springboot;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
